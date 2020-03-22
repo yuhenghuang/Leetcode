ce82e988d9f878344992b599817dae8fab472e2f
yuhenghuang <kongqiota@gmail.com>
2020-03-22 12:32:22 +0900

    253_heap

--- 253_MeetingRoomII.java
+++ 253_MeetingRoomII.java
@@ -1,12 +1,31 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.TreeMap;
 
 class MeetingRoom {
   public static void main(String[] args) {
-    int[][] itv = new int[][] {{0, 30}, {5,10}, {15,20}};
+    int[][] itv = new int[][] {{13, 15}, {1,13}};
     MeetingRoom obj = new MeetingRoom();
     System.out.println(obj.minMeetingRooms(itv));
   }
   public int minMeetingRooms(int[][] intervals) {
+    Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+    Queue<int[]> heap = new PriorityQueue<>(
+      (a,b) -> a[1] - b[1]
+    );
+
+    int res=0;
+    for (int[] meeting : intervals) {
+      while(!heap.isEmpty() && heap.peek()[1]<=meeting[0])
+        heap.poll();
+      heap.offer(meeting);
+      res=Math.max(res, heap.size());
+    }
+    return res;
+  }
+
+  public int minMeetingRoomsTreeMap(int[][] intervals) {
     TreeMap<Integer, Integer> treemap = new TreeMap<>();
     for (int[] meeting : intervals) {
       treemap.put(meeting[0], treemap.getOrDefault(meeting[0], 0)+1);
