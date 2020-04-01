0889e1c4e1b4ff36dad56b165d1dfe566159b260
yuhenghuang <kongqiota@gmail.com>
2020-04-01 21:33:19 +0900

    951

---
+++ 951_FlipEqTree.java
@@ -0,0 +1,11 @@
+class FlipEqTree {
+  public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+    if (root1==null && root2==null) return true;
+    else if (root1!=null && root2!=null) 
+      if (root1.val==root2.val && 
+         ((flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) || 
+         (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))))
+        return true;
+    return false;
+  }
+}
