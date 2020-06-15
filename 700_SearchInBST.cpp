980ba614046b943581e08abae93f76963871776b
yuhenghuang <kongqiota@gmail.com>
2020-06-16 08:26:10 +0900

    emmm

--- 700_SearchInBST.cpp
+++ 700_SearchInBST.cpp
@@ -4,7 +4,7 @@ class Solution {
   public:
     TreeNode* searchBST(TreeNode* root, int val) {
       if (root==nullptr) return nullptr;
-      if (root->val==val) return root;
+      else if (root->val==val) return root;
       else if (root->val>val) return searchBST(root->left, val);
       else return searchBST(root->right, val);
     }
